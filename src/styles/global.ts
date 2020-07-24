82208cb049d6d46e0a4e92bfc124053697f153f5
Emanuel <erbiet@outlook.com>
2020-07-23 22:39:23 -0300

    feat: dashboard style using styled components

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -12,7 +12,7 @@ export default createGlobalStyle`
 }
 
 body {
-  background: url(${githubBackground}) no-repeat 70% top;
+  background: #F0F0F5 url(${githubBackground}) no-repeat 70% top;
   -webkit-font-smoothing: antialiased;
 }
 
